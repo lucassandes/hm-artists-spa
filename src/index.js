d34dfcf7bdbeaef5ae71928a36026cb5cd974171
lucassandes <lucassandes@msn.com>
2018-07-21 19:28:26 -0300

    Add skeleton css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/index.css';
+import './css/normalize.css';
+import './css/skeleton.css';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
