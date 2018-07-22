406f40cc0be1f7ac57b47234247a403f5007be99
lucassandes <lucassandes@msn.com>
2018-07-22 11:06:03 -0300

    refactor on index js to fix component name

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './css/normalize.css';
 import './css/skeleton.css';
 
-import App from './App';
+import App from './App.jsx';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
